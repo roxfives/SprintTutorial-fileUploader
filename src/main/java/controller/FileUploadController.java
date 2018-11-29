0ec9f927984e445307de77e2062d0403636df155
roxfives <ricardo.chagas2@outlook.com>
2018-11-29 14:12:19 -0200

    Comments the controller class

--- src/main/java/controller/FileUploadController.java
+++ src/main/java/controller/FileUploadController.java
@@ -16,36 +16,75 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.io.IOException;
 import java.util.stream.Collectors;
 
+/**
+ * This class is responsible to handle every request to the server, from the welcome
+ * page and uploading files to errors thrown
+ * */
 @Controller
 public class FileUploadController {
-
+    // The object responsible for providing all features and properties related to the storage and management of the files
     private final StorageService storageService;
 
+    /**
+     * Constructor which receives the object responsible for the management of the files
+     *
+     * @param storageService object responsible for providing all features and properties
+     *                       related to the storage and management of the files
+     * */
     @Autowired
     public FileUploadController(StorageService storageService) {
         this.storageService = storageService;
     }
 
+    /**
+     * Method responsible for adding the list of already uploaded files to the root
+     * page (the welcome/index page). It is triggered on a GET request to the root
+     * page
+     *
+     * @param model the map with the page attributes
+     *
+     * @return
+     * */
     @GetMapping("/")
     public String listUploadedFiles(Model model) throws IOException {
 
         model.addAttribute("files", storageService.loadAll().map(
                 path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-                        "serveFile", path.getFileName().toString()).build().toString())
-                .collect(Collectors.toList()));
+                        "serveFile",
+                        path.getFileName().toString()).build().toString()
+                ).collect(Collectors.toList()));
 
         return "uploadForm";
     }
 
+    /**
+     * This method is responsible for handling the request for downloading a file from the server.
+     * It is triggered on a GET request to /file/<filename>, where filename is the name of the
+     * requested file.
+     *
+     * @param filename the name of the file to be sent from the server
+     *
+     * @return    the http response to the download request
+     * */
     @GetMapping("/files/{filename:.+}")
     @ResponseBody
     public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+        Resource file = storageService.loadAsResource(filename); // File to be sent
 
-        Resource file = storageService.loadAsResource(filename);
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
                 "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 
+    /**
+     * Method responsible for uploading a file to the server. It is triggered on a POST request
+     * to the root page
+     *
+     * @param file the file to be uploaded
+     * @param redirectAttributes the attributes of the redirect object. It is used to add a
+     *                           message to the page once the upload is complete
+     *
+     * @return    the path to which the page is redirected after uploading the file
+     * */
     @PostMapping("/")
     public String handleFileUpload(@RequestParam("file") MultipartFile file,
                                    RedirectAttributes redirectAttributes) {
@@ -57,9 +96,15 @@ public class FileUploadController {
         return "redirect:/";
     }
 
+    /**
+     * Method responsible for handling the request to download of a file that isn't in the server
+     *
+     * @param exc the exception object
+     *
+     * @return    the http response when the exception occurs
+     * */
     @ExceptionHandler(StorageFileNotFoundException.class)
     public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
         return ResponseEntity.notFound().build();
     }
-
 }
