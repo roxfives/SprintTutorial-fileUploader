441c1a34339dcb5fafdc90803fde6fe6034ed93b
roxfives <ricardo.chagas2@outlook.com>
2018-11-29 14:26:50 -0200

    Comments the class with the entry point of the application

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -9,14 +9,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+/**
+ * The class with the entry point of the application. It launches the server
+ * */
 @SpringBootApplication(scanBasePackageClasses = {StorageService.class, FileUploadController.class})
 @EnableConfigurationProperties(StorageProperties.class)
 public class Application {
-
+    /**
+     * The main method (entry point)
+     * */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    /**
+     * The Bean associated with the initialization of the application
+     *
+     * @param storageService the object that gives access to all features and data related to the
+     *                       properties, storage and management of files in the server
+     *
+     * @return    the method to initialize the application
+     * */
     @Bean
     CommandLineRunner init(StorageService storageService) {
         return (args) -> {
