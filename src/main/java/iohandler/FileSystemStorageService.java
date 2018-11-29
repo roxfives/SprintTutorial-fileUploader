bbb5d089a1641862b3db96efdd1ac37dd080dad2
roxfives <ricardo.chagas2@outlook.com>
2018-11-29 11:48:47 -0200

    Comments the StorageService interface and the class that implements it

--- src/main/java/iohandler/FileSystemStorageService.java
+++ src/main/java/iohandler/FileSystemStorageService.java
@@ -17,32 +17,44 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
 
+/**
+ * This class is responsible for the IO operations and management of the files being uploaded/downloaded
+ */
 @Service
 public class FileSystemStorageService implements StorageService {
 
     private final Path rootLocation;
 
-    @Autowired
+    /**
+    * Constructor which gets the path to the file to be handled
+     *
+     * @param properties object with information about the location to store the file
+    */
+    @Autowired //Annotations to warn the framework to inject StorageProperties
     public FileSystemStorageService(StorageProperties properties) {
         this.rootLocation = Paths.get(properties.getLocation());
     }
 
+    /**
+    * This method stores the file in local system of the server
+     *
+     * @param file the file to be stored
+    */
     @Override
     public void store(MultipartFile file) {
         String filename = StringUtils.cleanPath(file.getOriginalFilename());
+
         try {
             if (file.isEmpty()) {
                 throw new StorageException("Failed to store empty file " + filename);
             }
             if (filename.contains("..")) {
-                // This is a security check
-                throw new StorageException(
-                        "Cannot store file with relative path outside current directory "
-                                + filename);
+                //  Avoids saving above the directory the files are supposed to be saved
+                throw new StorageException("Cannot store file with relative path outside current directory " + filename);
             }
+
             try (InputStream inputStream = file.getInputStream()) {
-                Files.copy(inputStream, this.rootLocation.resolve(filename),
-                        StandardCopyOption.REPLACE_EXISTING);
+                Files.copy(inputStream, this.rootLocation.resolve(filename), StandardCopyOption.REPLACE_EXISTING);
             }
         }
         catch (IOException e) {
@@ -50,6 +62,11 @@ public class FileSystemStorageService implements StorageService {
         }
     }
 
+    /**
+     * This method loads all the files that have been uploaded (it just looks under one level of the download directory)
+     *
+     * @return a stream of Path objects
+     */
     @Override
     public Stream<Path> loadAll() {
         try {
@@ -63,16 +80,28 @@ public class FileSystemStorageService implements StorageService {
 
     }
 
+    /**
+     * This method returns the path to the filename given by concatenating the filename with this path
+     *
+     * @param filename the name of the file to the concatenated to this path
+     * */
     @Override
     public Path load(String filename) {
         return rootLocation.resolve(filename);
     }
 
+    /**
+     * This method returns the given file as a Resource object
+     *
+     * @param filename the name of the file to be returned as a resource. The String should contain
+     *                 only the base name of the file (not the path to it).
+     * */
     @Override
     public Resource loadAsResource(String filename) {
         try {
             Path file = load(filename);
             Resource resource = new UrlResource(file.toUri());
+
             if (resource.exists() || resource.isReadable()) {
                 return resource;
             }
@@ -87,11 +116,17 @@ public class FileSystemStorageService implements StorageService {
         }
     }
 
+    /**
+     * Deletes the given file. For directories, deletes everything under it as well.
+     * */
     @Override
     public void deleteAll() {
         FileSystemUtils.deleteRecursively(rootLocation.toFile());
     }
 
+    /**
+     * Initializes the directory structure for the server
+     * */
     @Override
     public void init() {
         try {
--- src/main/java/iohandler/StorageService.java
+++ src/main/java/iohandler/StorageService.java
@@ -6,11 +6,47 @@ import org.springframework.web.multipart.MultipartFile;
 import java.nio.file.Path;
 import java.util.stream.Stream;
 
+/**
+ * Interface that a class should implement to serve all the storage and directory management
+ * for the uploading server
+ * */
 public interface StorageService {
+    /**
+     * Initializes the directory structure for the server
+     * */
     void init();
+
+    /**
+     * This method stores the file in local system of the server
+     *
+     * @param file the file to be stored
+     */
     void store(MultipartFile file);
+
+    /**
+     * This method loads all the files that have been uploaded (it just looks under one level of the download directory)
+     *
+     * @return a stream of Path objects
+     */
     Stream<Path> loadAll();
+
+    /**
+     * This method returns the path to the filename given by concatenating the filename with this path
+     *
+     * @param filename the name of the file to the concatenated to this path
+     * */
     Path load(String filename);
+
+    /**
+     * This method returns the given file as a Resource object
+     *
+     * @param filename the name of the file to be returned as a resource. The String should contain
+     *                 only the base name of the file (not the path to it).
+     * */
     Resource loadAsResource(String filename);
+
+    /**
+     * Deletes the given file. For directories, deletes everything under it as well.
+     * */
     void deleteAll();
 }
